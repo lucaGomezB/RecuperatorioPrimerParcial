8615244154e30a34a024736a822f341abf329294
Luca Gomez <gomez.luca.007@gmail.com>
2025-06-02 14:53:06 -0300

    Empezado el proyecto

---
+++ RecuperatorioPrimerParcial/src/main/java/com/mycompany/recuperatorioprimerparcial/Main.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.recuperatorioprimerparcial;
+
+/**
+ *
+ * @author Skywalker
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
